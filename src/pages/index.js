91a2c3f88d6192f0430345638d818bfbd087129b
Kendall Strautman <kendallstraut@gmail.com>
2019-07-08 15:04:42 -0700

    pure version 'without gatsby' starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>A Lumberjack's Journal</h1>
-    <p>Notes on the arboreal universe.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => <div>Forestry Starter Blog</div>
 
 export default IndexPage
