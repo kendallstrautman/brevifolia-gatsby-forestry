70e6bfe9d937ea209dec276753be3e9f4addb024
Kendall Strautman <kendallstraut@gmail.com>
2019-08-02 10:41:44 -0700

    added yaml transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,11 +13,19 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-transformer-remark",
     "gatsby-plugin-react-helmet",
+    "gatsby-transformer-yaml",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "src",
         path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/src/assets/data`,
       },
     },
     {
